7d62cf78280d85ec0b691ecc7b9b69aa07a8903f
DESKTOP-V6ANAB8\user <lai225joseph@gmail.com>
2026-04-23 15:35:14 +0800

    LanguageTranslation_GermanToEnglish

--- LanguageTranslatoin_Seq2Seq_GermanToEnglish.py
+++ LanguageTranslatoin_Seq2Seq_GermanToEnglish.py
@@ -16,24 +16,25 @@ SOS_token = 0
 EOS_token = 1
 
 class Lang:
-      def __init__(self, name):
-          self.name = name
-          self.word2index = {}
-          self.word2count = {}
-          self.index2word = {0: "SOS", 1: "EOS"}
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  
 
-      def addSentence(self,sentence):
-         for word in sentence.split(' '):
-             self.addWord(word)
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
 
-      def addWord(self, word):
-         if word not in self.word2index:
+    def addWord(self, word):
+        if word not in self.word2index:
             self.word2index[word] = self.n_words
             self.word2count[word] = 1
             self.index2word[self.n_words] = word
             self.n_words += 1
-         else:
-               self.wrod2count[word] += 1
+        else:
+            self.word2count[word] += 1
 
 def normalizeString(s):
 
@@ -52,13 +53,14 @@ def readLangs(lang1, lang2, reverse=False):
 
     print("Readig lines...")
 
-    lines = open('datasets/data/%2-%s.txt' % (lang1, lang2), encoding = 'utf-8'). |
-    read().strip().split('\n')
+    lines = open('datasets/data/%s-%s.txt' % (lang1, lang2), encoding='utf-8'). \
+                  read().strip().split('\n')
+
     pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
 
     if reverse:
-       pairs = [list[reversed(p)) for p in pairs]
-       input_lang = lang(lang2)
+       pairs = [list(reversed(p)) for p in pairs]
+       input_lang = Lang(lang2)
        output_lang = Lang(lang1)
 
     else:
@@ -115,7 +117,7 @@ class EncoderRNN(nn.Module):
       
       def forward(self, input, hidden):
 
-          embedded = self.embedding(input),.view(1,1,-1)
+          embedded = self.embedding(input).view(1, 1, -1)
           output = embedded
 
           output, hidden = self.gru(output, hidden)
@@ -145,12 +147,12 @@ class DecoderRNN(nn.Module):
           return torch.zeros(1, 1, self.hidden_size)
       
 def tensorFromSentence(lang, sentence):
-    indexes = [lang.word2index[word] for word in sentence,.split(' ')]
+    indexes = [lang.word2index[word] for word in sentence.split(' ')]
 
     indexes.append(EOS_token)
     return torch.tensor(indexes, dtype=torch.long).view(-1, 1)
 
-def tensorFromPair(pair):
+def tensorsFromPair(pair):
     input_tensor = tensorFromSentence(input_lang, pair[0])
     target_tensor = tensorFromSentence(output_lang, pair[1])
 
@@ -158,51 +160,59 @@ def tensorFromPair(pair):
 
 teacher_forcing_ratio = 0.5
 
-def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
-     encoder_hidden = encoder.initHidden()
-
-     encoder_optimizer.zero_grad()
-     decoder_optimizer.zero_grad()
-
-     input_length = input_tensor.size(0)
-     target_length = target_tensor.size(0)
-
-     loss = 0
-  
-     for ei in range(input_length):
-        encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+def train(input_tensor, target_tensor, 
+          encoder, decoder,
+          encoder_optimizer, decoder_optimizer, 
+          criterion):
     
-     decoder_input = torch([[SOS_token]])
-     decoder_hidden = encoder_hidden
+    encoder_hidden = encoder.initHidden()
 
-     use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
-    
-     if use_teacher_forcing:
-     
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            input_tensor[ei], encoder_hidden)
+
+    decoder_input = torch.tensor([[SOS_token]])
+
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+       
         for di in range(target_length):
-            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden)
+            
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  
+
+    else:
+        
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden)
+            
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()  
 
             loss += criterion(decoder_output, target_tensor[di])
-            decoder_input = target_tensor[di]
+            if decoder_input.item() == EOS_token:
+                break
 
-     else:
+    loss.backward()
 
-          for di in range(target_length):
-              decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+    encoder_optimizer.step()
+    decoder_optimizer.step()
 
-              topv, topi = decoder_output.topk(1)
-              decoder_input = topi.squeeze().detach()
-
-              loss += criterion(decoder_output, target_tensor[di])
-              if decoder_input.item() == EOS_token:
-                 break
-
-          loss.backward()
-
-          encoder_optimizer.step()
-          decoder_optimizer.step()
-
-          return loss.item() / target_length
+    return loss.item() / target_length
      
 plot_losses = []
 print_loss_total = 0
@@ -213,33 +223,37 @@ encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
 decoder1 = DecoderRNN(hidden_size, output_lang.n_words)
 
 encoder_optimizer = optim.SGD(encoder1.parameters(), lr=0.01)
-decoder_optimizer = optim.SGD(decoder1.parameters(), lr=0.01(
+decoder_optimizer = optim.SGD(decoder1.parameters(), lr=0.01)
 
 training_pairs = [tensorsFromPair(random.choice(pairs))
-                  for i in range(3000)]
+                  for i in range(30000)]
 criterion = nn.NLLLoss()
 
 for iter in range(1, 30001):
+    
+    training_pair = training_pairs[iter - 1]
+    input_tensor = training_pair[0]
+    target_tensor = training_pair[1]
 
-     training_pair = training_pairs[iter - 1]
-     input_tensor = training_pair[0]
-     target_tensor = training_pair[1]
+    loss = train(input_tensor, target_tensor, 
+                 encoder1, decoder1, 
+                 encoder_optimizer, decoder_optimizer, 
+                 criterion)
+    
+    print_loss_total += loss
+    plot_loss_total += loss
 
-     loss = train(input_tensor, target_tensor, encoder1, decoder1, encoder_optimizer, decoder_optimizer, criterion)
-
-     print_loss_total += loss
-     plot_loss_total += loss
-
-     if iter % 10000 == 0:
-        print_loss_avg = print_loss_total /100
+    if iter % 1000 == 0:
+        print_loss_avg = print_loss_total / 100
         print_loss_total = 0
+        print('iteration - %d loss - %.4f' % (iter, print_loss_avg))
 
-     if iter % 100 == 0:
-        plot_loss_avg = plt_loss_total /100
+    if iter % 100 == 0:
+        plot_loss_avg = plot_loss_total / 100
         plot_losses.append(plot_loss_avg)
         plot_loss_total = 0
 
-fig, ax = plt.subplots(figsize(15, 8))
+fig, ax = plt.subplots(figsize=(15, 8))
 loc = ticker.MultipleLocator(base=0.2)
 ax.yaxis.set_major_locator(loc)
 plt.plot(plot_losses)
@@ -270,7 +284,7 @@ def evaluate(encoder, decoder, sentence):
                 break
              else:
                   decoded_words.append(output_lang.index2word[topi.item()])
-       return decoded_words
+         return decoded_words
     
 
 for i in range(10):
@@ -280,7 +294,7 @@ for i in range(10):
          print('>', pair[0])
          print('=', pair[1])
 
-         output_words = evaulate(encoder1, decoder1, pair[0])
+         output_words = evaluate(encoder1, decoder1, pair[0])
          output_sentence = ' '.join(output_words)
 
          print('<', output_sentence)
